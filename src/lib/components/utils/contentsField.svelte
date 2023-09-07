793472ca40b1ddc002285085caba8960da4843bc
Ryo <keyring1231@gmail.com>
2023-09-07 10:09:27 +0900

    水平方向のポジションを選べるようにした

--- src/lib/components/utils/contentsField.svelte
+++ src/lib/components/utils/contentsField.svelte
@@ -1,11 +1,34 @@
-<div class="contents-field">
-  <slot name="title" />
-  <slot />
+<script lang="ts">
+  export let horizontalPosition: "left" | "center" | "right" = "left";
+
+  let center = horizontalPosition === "center" ? true : false;
+  let right = horizontalPosition === "right" ? true : false;
+</script>
+
+<div 
+  class="contents-field"
+>
+  <div 
+    class:center
+    class:right
+  >
+    <slot />
+  </div>
 </div>
 
 <style>
   .contents-field {
     width: var(--content-width);
     margin-inline: auto;
+    display: grid;
+    align-items: baseline;
+  }
+
+  .center {
+    justify-self: center;
+  }
+
+  .right {
+    justify-self: end;
   }
 </style>
