5af45cbd853771da3e59ac82dad2a075985252f9
Ryo <keyring1231@gmail.com>
2023-09-07 15:40:55 +0900

    エラーメッセージの追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,37 @@
 	export let form;
 </script>
 
-<form method="POST">
-	<label>
-		<div class="title">パスフレーズを入力してね♪</div>
-		<input name="passphrase" autocomplete="off" />
-	</label>
-</form>
+<div class="container">
+	<div class="wrapper">
+		<form method="POST">
+			<label>
+				<div class="title">パスフレーズを入力してね♪</div>
+				<input name="passphrase" autocomplete="off" />
+			</label>
+		</form>
+		{#if form?.incorrect}
+			<p class="error">パスフレーズが違うみたいだよ。</p>
+		{/if}
+	</div>
+</div>
 
-{#if form?.incorrect}
-	<p class="error">パスフレーズが違うみたいだよ。</p>
-{/if}
 
 <style>
+	.container {
+		min-height: 100dvh;
+		height: 100%;
+		display: grid;
+		place-items: center;
+		background-color: rgb(48, 46, 46);
+		color: white;
+		font-size: 1.2rem;
+	}
 	.title {
 		margin-bottom: .5em;
+	}
+	.wrapper {
+		position: relative;
+		padding-inline: 10px;
 	}
 	label {
 		display: flex;
@@ -23,24 +40,18 @@
 		justify-content: center;
 		align-items: center;
 	}
-	form {
-		min-height: 100dvh;
-		height: 100%;
-		display: grid;
-		place-items: center;
-		background-color: black;
-		color: white;
-		font-size: 20px;
-
-	}
 	input {
 		width: 100%;
-		line-height: 2;
 		font: inherit;
 		padding: 0.5em;
 		border-radius: 10px;
 	}
 	.error {
+		padding:0;
+		margin: 0;
+		position: absolute;
+		bottom: -3em;
+		left: 0;
 		color: red;
 	}
 </style>
