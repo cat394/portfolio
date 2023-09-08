6170832dbd8b45efe9de625c684569fc97dd10c1
Ryo <keyring1231@gmail.com>
2023-09-08 11:57:51 +0900

    先ほどのテストを削除

--- src/lib/components/utils/form/input.svelte
+++ src/lib/components/utils/form/input.svelte
@@ -5,19 +5,10 @@
 	export let type = 'text';
 	export let required = true;
 	export let placeholder = 'Type text here';
-
-	let currentScrollY: number;
-
-	function handleFocus() {
-		currentScrollY = window.scrollY;
-		setTimeout(() => {
-			window.scrollTo(0, currentScrollY);
-		}, 0);
-	}
 </script>
 
 <Label {formName}>
 	<div>
-		<input on:focus={handleFocus} {type} {placeholder} {required} {...$$props} />
+		<input {type} {placeholder} {required} {...$$props} />
 	</div>
 </Label>
