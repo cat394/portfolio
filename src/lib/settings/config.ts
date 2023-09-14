0e42a27a8d585e83c497a6535511213b82e2ab5d
Ryo <keyring1231@gmail.com>
2023-09-14 09:50:04 +0900

    urlの修正

--- src/lib/settings/config.ts
+++ src/lib/settings/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment';
 
 export const title = 'Who is Ryo?';
 export const description = 'SvelteKit blog for poets';
-export const url = dev ? 'http://localhost:5173/' : 'https://ryo-webdev-netlify.app';
+export const url = dev ? 'http://localhost:5173/' : 'https://ryo-webdev-vercel.app';
